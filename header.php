80d0ec883c4c3bd7b16ad55c298b7a8905339617
thekirankumardash <thekirankumardash@gmail.com>
2016-01-09 19:05:41 +0530

    4.4 - Placing the header image behind the site title

--- header.php
+++ header.php
@@ -26,15 +26,20 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		
-		<?php if ( get_header_image() ) : ?>
-        <div class="header-image">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-            </a>
-        </div>
-        <?php endif; // End header image check. ?>
-        
-        <div class="site-branding">
+		<?php if ( get_header_image() && ('blank' == get_header_textcolor()) ) { ?>
+            <figure class="header-image">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <img src="<?php header_image(); ?>" width="< ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+                </a>
+            </figure>
+        <?php } // End header image check. ?>
+        <?php 
+            if ( get_header_image() && !('blank' == get_header_textcolor()) ) { 
+                echo '<div class="site-branding header-background-image" style="background-image: url(' . get_header_image() . ')">'; 
+            } else {
+                echo '<div class="site-branding">';
+            }
+        ?>
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
