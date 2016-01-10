975627b759db354e10764fd75de1c01bb839c2c0
thekirankumardash <thekirankumardash@gmail.com>
2016-01-10 20:27:59 +0530

    Chapter 12: Working with Static Pages. 12.1 - Styling pages

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,7 +10,18 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	
+	<?php 
+	if (has_post_thumbnail()) {
+		echo '<div class="single-post-thumbnail clear">';
+		echo '<div class="image-shifter">';
+		echo the_post_thumbnail('large-thumb');
+		echo '</div>';
+		echo '</div>';
+	}
+	?>
+    
+    <header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
