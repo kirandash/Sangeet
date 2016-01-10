1e211de355f21a810545f60f4c2c32c29e91ed8a
thekirankumardash <thekirankumardash@gmail.com>
2016-01-10 12:27:55 +0530

    7.6 - Using Masonry to make footer widgets responsive

--- functions.php
+++ functions.php
@@ -144,6 +144,8 @@ function sangeet_scripts() {
 	
 	wp_enqueue_script( 'sangeet-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_script( 'sangeet-masonry', get_template_directory_uri() . '/js/masonry-settings.js', array('masonry'), '20140401', true );
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
