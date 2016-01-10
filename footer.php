4b119d47aef569756d15bf6d81e2f944d86f56fa
thekirankumardash <thekirankumardash@gmail.com>
2016-01-10 11:35:59 +0530

    Chapter 7: Working with Widgetized Areas. 7.1 - Adding a widgetized area to the footer.

--- footer.php
+++ footer.php
@@ -14,7 +14,10 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+		
+        <?php get_sidebar( 'footer' ); ?>
+        
+        <div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sangeet' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'sangeet' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'sangeet' ), 'sangeet', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
--- functions.php
+++ functions.php
@@ -108,6 +108,16 @@ function sangeet_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+	
+	register_sidebar( array(
+		'name'          => __( 'Footer Widgets', 'sangeet' ),
+		'description'   => __( 'Footer widgets area appears in the footer of the site.', 'sangeet' ),
+		'id'            => 'sidebar-2',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 }
 add_action( 'widgets_init', 'sangeet_widgets_init' );
 
