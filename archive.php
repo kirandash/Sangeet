6fbbe0e0b01f1f79e2f52da0ff2b83f1f8756e73
thekirankumardash <thekirankumardash@gmail.com>
2016-01-10 15:47:31 +0530

    8.9 - Single post navigation

--- archive.php
+++ archive.php
@@ -35,7 +35,7 @@ get_header(); ?>
 
 			endwhile;
 
-			the_posts_navigation();
+			sangeet_paging_nav();
 
 		else :
 
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -139,3 +139,67 @@ function sangeet_social_menu() {
 	);
     }
 }
+
+if ( ! function_exists( 'sangeet_paging_nav' ) ) :
+/**
+ * Display navigation to next/previous set of posts when applicable.
+ *
+ * @return void
+ */
+function sangeet_paging_nav() {
+	// Don't print empty markup if there's only one page.
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
+	}
+	?>
+	<nav class="navigation paging-navigation" role="navigation">
+		<div class="post-nav-box clear">
+            <h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'sangeet' ); ?></h1>
+            <div class="nav-links">
+    
+                <?php if ( get_next_posts_link() ) : ?>
+                <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'sangeet' ) ); ?></div>
+                <?php endif; ?>
+    
+                <?php if ( get_previous_posts_link() ) : ?>
+                <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'sangeet' ) ); ?></div>
+                <?php endif; ?>
+    
+            </div><!-- .nav-links -->
+        </div>
+	</nav><!-- .navigation -->
+	<?php
+}
+endif;
+
+if ( ! function_exists( 'sangeet_post_nav' ) ) :
+/**
+ * Display navigation to next/previous post when applicable.
+ *
+ * @return void
+ */
+function sangeet_post_nav() {
+	// Don't print empty markup if there's nowhere to navigate.
+	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
+	$next     = get_adjacent_post( false, '', false );
+
+	if ( ! $next && ! $previous ) {
+		return;
+	}
+	?>
+	
+    <nav class="navigation post-navigation" role="navigation">
+        <div class="post-nav-box clear">
+            <h1 class="screen-reader-text"><?php _e( 'Post navigation', 'sangeet' ); ?></h1>
+            <div class="nav-links">
+                <?php
+                previous_post_link( '<div class="nav-previous"><div class="nav-indicator">' . _x( 'Previous Post:', 'Previous post', 'sangeet' ) . '</div><h1>%link</h1></div>', '%title' );
+                next_post_link(     '<div class="nav-next"><div class="nav-indicator">' . _x( 'Next Post:', 'Next post', 'sangeet' ) . '</div><h1>%link</h1></div>', '%title' );
+                ?>
+            </div><!-- .nav-links -->
+        </div><!-- .post-nav-box -->
+    </nav><!-- .navigation -->
+                    
+	<?php
+}
+endif;
